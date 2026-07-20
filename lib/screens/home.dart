f33c012f8664158e3498faf8dadeed87c2206fbc
Gourav Kumar Raut <bossgghere@users.noreply.github.com>
2026-07-20 10:12:18 +0530

    Expand keypad size to cover 60% of screen height

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -23,7 +23,7 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: LayoutBuilder(
           builder: (context, constraints) {
-            double calculatedRowHeight = (constraints.maxHeight * 0.11).clamp(44.0, 75.0);
+            double calculatedRowHeight = (constraints.maxHeight * 0.12).clamp(42.0, 95.0);
 
             return Column(
               children: [
